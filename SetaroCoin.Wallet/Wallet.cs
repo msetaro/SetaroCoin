b72a04bdbc3c7ae5e561860582ab7726382597e3
Matt Setaro <fouulsy@gmail.com>
2024-09-07 11:05:42 -0400

    saving for later

--- SetaroCoin.Wallet/Wallet.cs
+++ SetaroCoin.Wallet/Wallet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,7 @@ namespace SetaroCoin.Wallet;
 
 public class UserWallet
 {
+    public string Address => BitConverter.ToString(_address);
+    private readonly byte[] _address = SHA256.HashData(Guid.NewGuid().ToByteArray());
 }
 
--- SetaroCoin/Models/Transaction.cs
+++ SetaroCoin/Models/Transaction.cs
@@ -16,12 +16,45 @@ public class Transaction
     public byte[] Hash => this.ToBytes();
 
     /// <summary>
+    /// A Guid for this transaction
+    /// </summary>
+    public Guid TransactionId => _guid;
+    private readonly Guid _guid = Guid.NewGuid();
+
+    /// <summary>
     /// Wallet of the sender.
     /// </summary>
-    public UserWallet Sender { get; set; }
+    public required UserWallet Sender { get; init; }
 
     /// <summary>
     /// Wallet of the recipient.
     /// </summary>
-    public UserWallet Recipient { get; set; }
+    public required UserWallet Recipient { get; init; }
+
+    /// <summary>
+    /// The amount being transacted between sender and recipient.
+    /// </summary>
+    public required float Amount { get; init; }
+
+    /// <summary>
+    /// The digital signature from the sender for this transaction.
+    /// </summary>
+    public required byte[] SenderSignature { get; init; }
+
+    /// <summary>
+    /// The amount being charged as a network fee.
+    /// </summary>
+    public float Fee { get; set; }
+
+    /// <summary>
+    /// If the transaction has been added to a mined block. i.e has been confirmed.
+    /// </summary>
+    public bool IsConfirmed { get; set; }
+
+    /// <summary>
+    /// The block(s) this transaction was confirmed in.
+    /// </summary>
+    public Block[] ConfirmedIn { get; set; } = [];
+
+
 }
